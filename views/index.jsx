7098fc78ab95db3cebd9a860d3909d040d3bcb38
Aishwarya Sai <aishwaryasaibetha@gmail.com>
2017-11-12 21:16:41 +0530

    Login done- registration err handle

---
+++ views/index.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var DefaultLayout = require('./layouts/defaultHome');
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <DefaultLayout>
+        <div>
+
+        </div>
+      </DefaultLayout>
+    );
+  }
+}
+module.exports = HelloMessage;
